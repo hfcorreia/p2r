904016aa8dd3550b695ee3897028945b1ade754d
Hugo Correia <hfcorreia91@gmail.com>
2014-03-04 19:00:07 +0000

    Add example test suite

--- run-tests.rkt
+++ run-tests.rkt
@@ -1,6 +1,25 @@
 #lang racket
 
 (require rackunit
-         "lexer.rkt"
-         "parser.rkt"
-         "ast.rkt")
+         rackunit/text-ui)
+
+(run-tests
+(test-suite
+ "Test integer literals"
+ 
+ 
+ #:before (lambda () (displayln "Testing integer literals"))
+
+ (test-case
+  "Decimal tests"
+  (check-eq? 1 2))
+ 
+ (test-case
+  "Decimal tests2"
+  (check-eq? 1 2))))
+
+; Test with whitespaces, CR, line feed, etc
+; Test with octal, hexadecimal, etc notations
+; Test with test with parentisis
+; Test with delimitator(Semicolon,etc)
+; Test with variable atribution
