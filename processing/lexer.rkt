3463daa7baf667d5d0c8bcbb204f9f64beaf6d26
Hugo Correia <hfcorreia91@gmail.com>
2014-07-09 17:41:43 +0100

    Add comment support in lexer

--- processing/lexer.rkt
+++ processing/lexer.rkt
@@ -2,65 +2,66 @@
   (require parser-tools/lex
            (prefix-in re: parser-tools/lex-sre)
            racket/string)
-  
+
   (provide (all-defined-out))
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;;; Tokens definitions
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (define-empty-tokens operators 
-    (=    >    <    !     ~    ?     :
-          ==   <=   >=   !=    &&   ||    ++    --
-          +    -    *    /     &    |     ^     %     <<     >>     >>>
-          +=   -=   *=   /=    &=   |=    ^=    %=    <<=    >>=    >>>=))
-  
+                       (=    >    <    !     ~    ?     :
+                             ==   <=   >=   !=    &&   ||    ++    --
+                             +    -    *    /     &    |     ^     %     <<     >>     >>>
+                             +=   -=   *=   /=    &=   |=    ^=    %=    <<=    >>=    >>>=))
+
   (define-empty-tokens keywords
-    (abstract    continue    for          new          switch
-                 assert      default     if            package      synchronized
-                 boolean     do          goto          private      this
-                 break       double      implements    protected    throw
-                 byte        else        import        public       throws
-                 case        enum        instanceof    return       transient
-                 catch       extends     int           short        try
-                 char        final       interface     static       void 
-                 class       finally     long          strictfp     volatile
-                 const       float       native        super        while))
-  
+                       (abstract    continue    for          new          switch
+                                    assert      default     if            package      synchronized
+                                    boolean     do          goto          private      this
+                                    break       double      implements    protected    throw
+                                    byte        else        import        public       throws
+                                    case        enum        instanceof    return       transient
+                                    catch       extends     int           short        try
+                                    char        final       interface     static       void 
+                                    class       finally     long          strictfp     volatile
+                                    const       float       native        super        while))
+
   (define-empty-tokens seperators
-    (semicolon period comma
-               l-paren  r-paren
-               l-cbrack r-cbrack
-               l-sbrack r-sbrack))
-  
+                       (semicolon period comma
+                                  l-paren  r-paren
+                                  l-cbrack r-cbrack
+                                  l-sbrack r-sbrack))
+
   (define-empty-tokens empty-literals 
-    (EOF null-lit))
-  
+                       (EOF null-lit))
+
   (define-tokens literals 
-    (identifier integer-lit float-lit double-lit char-lit boolean-lit string-lit))
-  
+                 (identifier integer-lit float-lit double-lit char-lit boolean-lit string-lit))
+
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;;; Token abbreviations exapanded by the lexer
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (define-lex-abbrevs
-    
+    (input-charcter (re:~ #\return #\linefeed))
+
     ;; numerals and suffix
     (digits      (re:+ (re:/ "09")))
     (float-suf   (char-set "fF"))
     (double-suf  (char-set "dD"))
     (long-suf    (char-set "lL"))
     (hex-digit   (re:/ "09" "af" "AF"))
-    
+
     ;; integer literals
     (binary      (re:: #\0 "b" (re:+ (re:/ "01"))))
     (octal       (re:: #\0 (re:+ (re:/ "07"))))
     (hexa        (re:: #\0 (char-set "xX") (re:+ hex-digit )))
     (decimal     (re:or #\0 (re:: (re:/ "19") (re:* (re:/ "09")))))
-    
+
     ;; float literals
     (float-a     (re:: digits #\. (re:? digits) (re:? exponent)))
     (float-b     (re:: #\. digits (re:? exponent)))
     (float-c     (re:: digits (re:? exponent)))
     (exponent    (re:: (char-set "eE") (re:? (char-set "+-")) digits))
-    
+
     ;; char literals
     (char        (re:: #\' (re:~ whitespace #\' #\\) #\'))
     (escape-seq  (re:or "\\b" "\\t" "\\n" "\\f" "\\r" "\\\"" "\\'" "\\\\"
@@ -68,7 +69,7 @@
                         (re:: #\\ (re:/ "07"))))
     ;; string literals
     (string      (re:: #\" (re:* (re:~ #\" )) #\"))
-    
+
     ;; keywords
     (keyword     (re:or "abstract"    "continue"    "for"           "new"          "switch"
                         "assert"      "default"     "if"            "package"      "synchronized"
@@ -80,125 +81,144 @@
                         "char"        "final"       "interface"     "static"       "void"
                         "class"       "finally"     "long"          "strictfp"     "volatile"
                         "const"       "float"       "native"        "super"        "while"))
-    
+
     ;; operator
     (operator   (re:or "="    ">"    "<"    "!"     "~"    "?"     ":"
                        "=="   "<="   ">="   "!="    "&&"   "||"    "++"    "--"
-                       "+"    "-"    "*"   "/"     "&"    "|"     "^"     "%"     "<<"     ">>"     ">>>"
-                       "+="   "-="   "*="  "/="    "&="   "|="    "^="    "%="    "<<="    ">>="    ">>>="))
-    
+                       "+"    "-"    "*"   "/"     "&"    "|"     
+                       "^"     "%"     "<<"     ">>"     ">>>"
+                       "+="   "-="   "*="  "/="    "&="   "|="    
+                       "^="    "%="    "<<="    ">>="    ">>>="))
+
     ;; identifier
-    (identifier     (re:: (re:or (re:/ "Az" "az") "_" "$")
-                          (re:*  (re:or (re:/ "Az" "az" "09") "_" "$")))) 
+    (identifier  (re:: (re:or (re:/ "Az" "az") "_" "$")
+                       (re:*  (re:or (re:/ "Az" "az" "09") "_" "$")))) 
+
+    ;; comments
+    (line-comment    (re:: "//" (re:* input-charcter) ))
+
     )
-  
+
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;;; Lexer definition
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (define lex
     (lexer-src-pos
-     ;; whitespaces, linefeeds, newline, etc
-     ((re:+ whitespace) (return-without-pos (lex input-port)))
-     ((re:+ blank)      (return-without-pos (lex input-port)))
-     
-     ;; seperators
-     (";"      (token-semicolon))
-     ("."      (token-period))
-     (","      (token-comma))
-     ("("      (token-l-paren))
-     (")"      (token-r-paren))
-     ("{"      (token-l-cbrack))
-     ("}"      (token-r-cbrack))
-     ("["      (token-l-sbrack))
-     ("]"      (token-r-sbrack))
-     
-     ;; operators
-     (operator     (string->symbol lexeme))
-     
-     ;; boolean
-     ("true"   (token-boolean-lit #t))
-     ("false"  (token-boolean-lit #f))
-     
-     ;; integers
-     (binary     
-      (token-integer-lit (string->number (trim-string lexeme 2 0) 2)))
-     (octal      
-      (token-integer-lit (string->number lexeme 8)))
-     (hexa       
-      (token-integer-lit (string->number (trim-string lexeme 2 0) 16)))
-     (decimal    
-      (token-integer-lit (string->number lexeme 10)))
-     ((re:: decimal long-suf)       
-      (token-integer-lit (string->number (trim-string lexeme 0 1) 10)))
-     ((re:: hexa long-suf)
-      (token-integer-lit (string->number (trim-string lexeme 2 1) 16)))
-     ((re:: octal long-suf)
-      (token-integer-lit (string->number (trim-string lexeme 0 1)  8)))
-     
-     ;; floats
-     ((re:: (re:or float-a float-b float-c) float-suf)
-      (token-float-lit (string->number (trim-string lexeme 0 1) 10)))
-     ((re:: (re:or float-a float-b float-c) double-suf)
-      (token-double-lit (string->number (trim-string lexeme 0 1) 10)))
-     
-     ;; chars
-     (char            (token-char-lit (string-ref lexeme 1)))
-     ((re:: #\' escape-seq #\')   
-      (token-char-lit (escape->char (trim-string lexeme 1 1))))
-     
-     ;; strings
-     (string          (token-string-lit (build-string lexeme)))
-     
-     ;; null
-     ("null"          (token-null-lit))
-     
-     ;; keywords
-     (keyword        (string->symbol lexeme))
-     
-     ;; identifiers
-     (identifier     (token-identifier lexeme))
-     
-     ;; terminator
-     ((eof)    (token-EOF))))
-  
+      ;; whitespaces, linefeeds, newline, etc
+      ((re:+ whitespace)    (return-without-pos (lex input-port)))
+      ((re:+ blank)         (return-without-pos (lex input-port)))
+      (line-comment         (return-without-pos (lex input-port)))
+      ("/*"                 (begin (comment-lex input-port)
+                                   (return-without-pos (lex input-port))))
+
+      ;; seperators
+      (";"      (token-semicolon))
+      ("."      (token-period))
+      (","      (token-comma))
+      ("("      (token-l-paren))
+      (")"      (token-r-paren))
+      ("{"      (token-l-cbrack))
+      ("}"      (token-r-cbrack))
+      ("["      (token-l-sbrack))
+      ("]"      (token-r-sbrack))
+
+      ;; operators
+      (operator     (string->symbol lexeme))
+
+      ;; boolean
+      ("true"   (token-boolean-lit #t))
+      ("false"  (token-boolean-lit #f))
+
+      ;; integers
+      (binary     
+        (token-integer-lit (string->number (trim-string lexeme 2 0) 2)))
+      (octal      
+        (token-integer-lit (string->number lexeme 8)))
+      (hexa       
+        (token-integer-lit (string->number (trim-string lexeme 2 0) 16)))
+      (decimal    
+        (token-integer-lit (string->number lexeme 10)))
+      ((re:: decimal long-suf)       
+       (token-integer-lit (string->number (trim-string lexeme 0 1) 10)))
+      ((re:: hexa long-suf)
+       (token-integer-lit (string->number (trim-string lexeme 2 1) 16)))
+      ((re:: octal long-suf)
+       (token-integer-lit (string->number (trim-string lexeme 0 1)  8)))
+
+      ;; floats
+      ((re:: (re:or float-a float-b float-c) float-suf)
+       (token-float-lit (string->number (trim-string lexeme 0 1) 10)))
+      ((re:: (re:or float-a float-b float-c) double-suf)
+       (token-double-lit (string->number (trim-string lexeme 0 1) 10)))
+
+      ;; chars
+      (char            (token-char-lit (string-ref lexeme 1)))
+      ((re:: #\' escape-seq #\')   
+       (token-char-lit (escape->char (trim-string lexeme 1 1))))
+
+      ;; strings
+      (string          (token-string-lit (build-string lexeme)))
+
+      ;; null
+      ("null"         (token-null-lit))
+
+      ;; keywords
+      (keyword        (string->symbol lexeme))
+
+      ;; identifiers
+      (identifier     (token-identifier lexeme))
+
+      ;; terminator
+      ((eof)    (token-EOF))))
+
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  ;;; String lexer
+  ;;; comment lexer
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  (define-tokens str-tokens
-    (str-end str-char))
-  
-  (define-empty-tokens empty-str-tokens
-    (str-eof))
-  
+  (define comment-lex
+    (lexer-src-pos
+      ("*/" end-pos)
+      ((eof) end-pos)
+      ((re:or "*" "/" (complement (re:: any-string (re:or "*" "/") any-string)))
+       (comment-lex input-port))
+      ((special) (comment-lex input-port))
+      ((special-comment) (comment-lex input-port))))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;; string lexer
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (define-tokens str-tokens (str-end str-char))
+
+  (define-empty-tokens empty-str-tokens (str-eof))
+
   (define (build-string input)
     (define (build-string-aux input-port)
       (let ((token (position-token-token (string-lex input-port))))
         (if (eq? (token-name token)
                  'str-eof)
-            (list)
-            (let ((value (if (char? (token-value token))
-                             (make-string 1 (token-value token))
-                             (token-value token))))
-              (cons value (build-string-aux input-port))))))
+          (list)
+          (let ((value (if (char? (token-value token))
+                         (make-string 1 (token-value token))
+                         (token-value token))))
+            (cons value (build-string-aux input-port))))))
     (string-append* "" (build-string-aux (open-input-string input))))
-  
+
   (define string-lex
     (lexer-src-pos
-     (#\"          (return-without-pos (string-lex input-port)))
-     (escape-seq   (token-str-char (escape->char lexeme)))
-     ((re:~ #\")   (token-str-char lexeme))
-     (whitespace   (token-str-char lexeme))
-     (blank        (token-str-char lexeme))
-     ((eof)        (token-str-eof))))
-  
+      (#\"          (return-without-pos (string-lex input-port)))
+      (escape-seq   (token-str-char (escape->char lexeme)))
+      ((re:~ #\")   (token-str-char lexeme))
+      (whitespace   (token-str-char lexeme))
+      (blank        (token-str-char lexeme))
+      ((eof)        (token-str-eof))))
+
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;;; Aux funtions
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  
+
   ;;; Trims a string given 2 offsets
   (define (trim-string string left right)
     (substring string left (- (string-length string) right)))
-  
+
   ;;; Converts escape sequences to char
   (define (escape->char es)
     (cond
