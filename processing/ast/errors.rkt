b613e9e21731683b50e84d9989f7fd928b7c23b9
Hugo Correia <hfcorreia91@gmail.com>
2015-02-05 16:15:43 +0000

    Add binary-error

--- processing/ast/errors.rkt
+++ processing/ast/errors.rkt
@@ -5,3 +5,6 @@
 
 (define (type-conversion-error obj t1 t2)
   (send obj type-error (format "Cannot convert a ~a to ~a" t1 t2)))
+
+(define (binary-error obj t1 t2)
+  (send obj type-error (format "Binary operation error ~a to ~a" t1 t2)))
